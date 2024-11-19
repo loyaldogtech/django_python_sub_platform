6be1f1601cfa5a1820f662589ca09dee1c81b159
Shad Spreiter <loyaldogtech@gmail.com>
2024-11-19 12:43:45 -0800

    Integrate user authentication completed

--- subplatform/account/views.py
+++ subplatform/account/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 
 def home(request):
@@ -6,9 +10,49 @@ def home(request):
     return render(request, 'account/index.html')
 
 def register(request):
-    
-    return render(request, 'account/register.html')
+
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('my_login')
+
+    context = {'RegisterForm': form}
+
+    return render(request, 'account/register.html', context)
+
 
 def my_login(request):
 
-    return render(request, 'account/my_login.html')
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')  # username is the email
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None and user.is_software_engineer == True:
+
+                login(request, user)
+                return HttpResponse('Welcome Software Engineer')
+            
+            if user is not None and user.is_software_engineer == False:
+
+                login(request, user)
+                return HttpResponse('Welcome Client')
+            
+    context = {'LoginForm': form}
+    return render(request, 'account/my_login.html', context)
+
+
+
+
+
+
