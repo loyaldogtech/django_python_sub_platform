ccdc48454dd9f876e0452fcae6956bd7de8f6ca2
Shad Spreiter <loyaldogtech@gmail.com>
2024-11-25 12:14:03 -0800

    logout of an account

--- subplatform/account/urls.py
+++ subplatform/account/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
    path('', views.home, name=''),
    path('register', views.register, name='register'),
    path('my_login', views.my_login, name='my_login'),
+   path('user_logout', views.user_logout, name='user_logout'),
    
    ]
