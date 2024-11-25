fd3a42f334bc25292d3e007c3927630334837422
Shad Spreiter <loyaldogtech@gmail.com>
2024-11-25 13:44:19 -0800

    protect views

--- subplatform/client/views.py
+++ subplatform/client/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='my_login')
 def client_dashboard(request):
 
     return render(request, 'client/client-dashboard.html')
--- subplatform/software_engineer/views.py
+++ subplatform/software_engineer/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='my_login')
 def software_engineer_dashboard(request):
 
     return render(request, 'software_engineer/software_engineer-dashboard.html')
