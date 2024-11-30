0e125f0081aabb05f30274bf58cf7398c9b08f7f
Shad Spreiter <loyaldogtech@gmail.com>
2024-11-29 20:37:23 -0800

    Create and Article

--- subplatform/software_engineer/urls.py
+++ subplatform/software_engineer/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
    
     path('software_engineer-dashboard', views.software_engineer_dashboard, name='software_engineer-dashboard'),
+    path('create-article', views.create_article, name='create-article'),
 ]
--- subplatform/software_engineer/views.py
+++ subplatform/software_engineer/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from . forms import ArticleForm
+from django.http import HttpResponse
 
 @login_required(login_url='my_login')
 def software_engineer_dashboard(request):
 
     return render(request, 'software_engineer/software_engineer-dashboard.html')
+
+@login_required(login_url='my_login')
+def create_article(request):
+
+    form = ArticleForm()
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return HttpResponse('Article created successfully')
+
+    context = {'CreateArticleForm': form}
+    return render(request, 'software_engineer/create-article.html', context)
