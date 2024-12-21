f900d6fdc27266736bba29e0d24fe2094cd25940
Shad Spreiter <loyaldogtech@gmail.com>
2024-12-20 19:33:55 -0800

    Account management - update account details

--- subplatform/software_engineer/forms.py
+++ subplatform/software_engineer/forms.py
@@ -1,8 +1,18 @@
 from . models import Article
+from account.models import CustomUser
 from django.forms import ModelForm
 
 class ArticleForm(ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'content', 'is_premium',]
+
+class UpdateUserForm(ModelForm):
+
+    password = None
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name']
+        exclude = ['password1', 'password2']
     
--- subplatform/software_engineer/views.py
+++ subplatform/software_engineer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from . forms import ArticleForm
+from . forms import ArticleForm, UpdateUserForm
 from django.http import HttpResponse
 from . models import Article
 
@@ -67,5 +67,19 @@ def delete_article(request, pk):
         article.delete()
         return redirect('my-articles')
     return render(request, 'software_engineer/delete-article.html')
+
+@login_required(login_url='my_login')
+def account_management(request):
+
+    form = UpdateUserForm(instance=request.user)
+
+    if request.method == 'POST':
+        form = UpdateUserForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('software_engineer-dashboard')
+
+    context = {'UpdateUserForm': form}
+    return render(request, 'software_engineer/account-management.html', context)
 
         
